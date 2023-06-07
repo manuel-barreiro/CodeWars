3b67f355b252b5259672b5adc209e24f1208a9c4
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-06-07 10:05:27 -0300

    makeEveryoneHappy.js

---
+++ 6kyu/makeEveryoneHappy.js
@@ -0,0 +1,33 @@
+// Overview
+// We often use smiley faces in correspondence with other people. They allow us to quickly show our reaction to the person(s) we are talking to.
+
+// But one day you wanted to make your correspondence more joyful. So today you have the opportunity to make it happen.
+
+// Task:
+// In this kata, your task will be to replace sad emoticons with funny ones.
+
+// The emoticons, will be represented from:
+
+// Eyes: marked as :, ; or =
+// Nose (optional): marked as - or ~
+// Mouth: marked as ( or [
+// Examples:
+// smile("Howdy :(")  // should return "Howdy :)"
+// smile("Never be sad =[")  // should return "Never be sad =]"
+// smile("Change this `=(` and this `:[`")  // should return "Change this `=)` and this `:]`"
+
+
+function smile(text) {
+    let strList = text.split('')
+  
+    for (let i = 0; i < strList.length; i++){
+        if ( (strList[i] === '(') && (strList[i-1] === ':' || strList[i-1] === ';' || strList[i-1] === '=' || strList[i-1] === '-' || strList[i-1] === '~') ) {
+            strList[i] = ')'
+        }
+        else if ( (strList[i] === '[') && (strList[i-1] === ':' || strList[i-1] === ';' || strList[i-1] === '=' || strList[i-1] === '-' || strList[i-1] === '~') ) {
+            strList[i] = ']'
+        }
+    }
+  
+    return strList.join('')
+  }
